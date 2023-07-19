3272e66769e6914c58b0f1b77da26f0d87bae3d8
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-07-19 19:58:23 +0600

    Add files via upload

--- linechart/src/CustomLineChart.tsx
+++ linechart/src/CustomLineChart.tsx
@@ -3,10 +3,11 @@ import { AxisLeft, AxisRight, AxisBottom } from "@visx/axis";
 import { scaleLinear, scaleBand } from "@visx/scale";
 import { LinePath } from "@visx/shape";
 import { Group } from "@visx/group";
-import { withTooltip, Tooltip } from "@visx/tooltip";
+import { Tooltip, defaultStyles } from "@visx/tooltip";
 import { WithTooltipProvidedProps } from "@visx/tooltip/lib/enhancers/withTooltip";
 // import "./CustomLineChart.css";
 import { MouseEvent } from "react";
+import { Circle } from "@visx/shape";
 
 interface DataPoint {
   name: string;
@@ -29,20 +30,8 @@ interface CustomLineChartProps {
     left: number;
     right: number;
   };
-  // yRange: number[];
   xDomain: string[];
-  // tooltipOpen: boolean;
-  // tooltipLeft: number;
-  // tooltipTop: number;
-  // tooltipData: DataPoint | undefined;
-  // hideTooltip: () => void;
-  // showTooltip: (event: React.MouseEvent<Element, MouseEvent>, data: DataPoint) => void;
 }
-
-const formatLabel = (label: string): string => {
-  const [day, time] = label.split(" ");
-  return `${day}\n${time}`;
-};
 
 export function CustomLineChart({
   lines,
@@ -50,58 +39,31 @@ export function CustomLineChart({
   height,
   margin,
   xDomain,
-}: // tooltipOpen,
-// tooltipLeft,
-// tooltipTop,
-// tooltipData,
-// hideTooltip,
-// showTooltip,
-CustomLineChartProps) {
-  const [tooltipData, setTooltipData] = useState<DataPoint | undefined>(
-    undefined
-  );
-  const [tooltipLeft, setTooltipLeft] = useState<number>(0);
-  const [tooltipTop, setTooltipTop] = useState<number>(0);
-  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
-
-  const showTooltip = (
-    event: React.MouseEvent<SVGPathElement, globalThis.MouseEvent>,
-    data: DataPoint
-  ) => {
-    const { clientX, clientY } = event;
-    setTooltipData(data);
-    setTooltipLeft(clientX);
-    setTooltipTop(clientY);
-    setTooltipOpen(true);
-  };
-
-  const hideTooltip = () => {
-    setTooltipOpen(false);
-  };
-
+}: CustomLineChartProps) {
+  const [tooltipData, setTooltipData] = useState<DataPoint | null>(null);
 
   const xMax = width - margin.left - margin.right;
   const yMax = height - margin.top - margin.bottom;
-
-
   const xScale = scaleBand<string>({
     domain: xDomain, // Use xDomain prop
     range: [-margin.left - 28, width + 14],
     padding: 1,
   });
 
-  const yMin = 0;
+  const yMin = 0.0;
   const yMaxi =
-    Math.max(...lines.flatMap((line) => line.data.map((d) => d.value))) + 0.2;
+    Math.max(...lines.flatMap((line) => line.data.map((d) => d.value))) + 0.6;
 
   const yScale = scaleLinear<number>({
     domain: [yMin, yMaxi],
     range: [height - margin.bottom, margin.top],
   });
 
-const tickValues = Array.from(
+  const tickValues = Array.from(
     new Set(yScale.ticks().map((value) => Math.ceil(value * 20) / 10))
-  ).filter((tick) => tick !== 0 && tick <= Math.ceil(yScale.domain()[1] * 10) / 10);
+  ).filter(
+    (tick) => tick !== 0 && tick <= Math.ceil(yScale.domain()[1] * 10) / 10
+  );
 
   // const averageValue = lines[0].data.reduce((sum, data) => sum + data.value, 0) / lines[0].data.length;
   const titleAverages = lines.map((line) => {
@@ -109,22 +71,6 @@ const tickValues = Array.from(
       line.data.reduce((sum, data) => sum + data.value, 0) / line.data.length;
     return { title: line.title, average };
   });
-
-  // const handleMouseHover = (
-  //   event: React.MouseEvent<SVGRectElement, MouseEvent>,
-  //   data: DataPoint
-  // ) => {
-  //   const { clientX, clientY } = event;
-  //   showTooltip({
-  //     tooltipData: data,
-  //     tooltipLeft: clientX,
-  //     tooltipTop: clientY,
-  //   });
-  // };
-
-  // const handleMouseLeave = () => {
-  //   hideTooltip();
-  // };
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -146,7 +92,7 @@ const tickValues = Array.from(
         ))}
       </div>
 
-      <svg width={width} height={height} onMouseLeave={hideTooltip}>
+      <svg width={width} height={height}>
         <rect
           x={margin.left}
           y={margin.top}
@@ -155,7 +101,6 @@ const tickValues = Array.from(
           fill="none"
           stroke="gray"
         />
-
 
         <g transform={`translate(0, ${height - margin.bottom})`}>
           {xScale.domain().map((value, index) => (
@@ -171,8 +116,6 @@ const tickValues = Array.from(
             </text>
           ))}
         </g>
-
-
 
         <g transform={`translate(${width - margin.right}, 0)`}>
           {tickValues.map((value, index) => (
@@ -199,24 +142,69 @@ const tickValues = Array.from(
               stroke={line.color}
               strokeWidth={2}
               strokeOpacity={0.8}
-              // onMouseMove={(event) => handleMouseHover(event, line.data)}
-              // onMouseLeave={handleMouseLeave}
-              onMouseMove={(event) => showTooltip(event, line.data[0])}
-              onMouseLeave={hideTooltip}
             />
           ))}
-        </Group>
 
-        {tooltipOpen && tooltipData && (
-          <Tooltip
-            top={tooltipTop}
-            left={tooltipLeft}
-            style={{ backgroundColor: "white", color: "#333" }}
+          {/* Render circles/points with tooltips */}
+          {lines.map((line) =>
+            line.data.map((d, index) => (
+              <g
+                key={index}
+                onMouseEnter={() => setTooltipData(d)}
+                onMouseLeave={() => setTooltipData(null)}
+              >
+                <Circle
+                  cx={xScale(d.name) ?? 0}
+                  cy={yScale(d.value)}
+                  r={3} // Adjust the size of the circles
+                  fill={line.color}
+                />
+              </g>
+            ))
+          )}
+        </Group>
+        {tooltipData && (
+          <g
+            transform={`translate(${xScale(tooltipData.name)! + margin.left -40},${
+              yScale(tooltipData.value) + margin.top - 60
+            })`}
           >
-            <strong>{tooltipData.name}</strong>
-            <br />
-            Value: {tooltipData.value.toFixed(2)}
-          </Tooltip>
+            <rect
+              x={-40}
+              y={-25}
+              width={90}
+              height={60}
+              fill="#f9f9f9"
+              stroke="gray"
+              strokeWidth={2}
+              rx={5}
+            />
+            <text
+              x={0}
+              y={-12}
+              fontSize={10}
+              textAnchor="middle"
+              dominantBaseline="middle"
+            >
+              {/* Display the title of the line */}
+              <tspan
+                x={0}
+                dy={5} // First line, adjust the dy value for spacing
+                fill={
+                  lines.find((line) => line.data.includes(tooltipData))?.color
+                }
+              >
+                {lines.find((line) => line.data.includes(tooltipData))?.title}
+              </tspan>
+              <tspan x={0} dy={15}>
+                {/* tooltipData.name */}
+                {tooltipData.name}
+              </tspan>
+              <tspan x={0} dy={15} fontWeight="bold">
+                {tooltipData.value}
+              </tspan>
+            </text>
+          </g>
         )}
       </svg>
     </div>
